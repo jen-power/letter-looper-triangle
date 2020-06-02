86f070c1bc458df08a311cec0fc0282f062dbfa0
Jen Power <powe0309@algonquinlive.com>
2020-06-02 15:01:06 -0400

    Add semi colon

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ var response = prompt('Enter a letter');
 
 var i;
 
-var triangle = response + response
+var triangle = response + response;
 
 for (i = 0; i < 10; i++) {
   document.write (triangle + '<br>');
